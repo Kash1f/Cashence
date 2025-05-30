2560f62e15b089f8e11f261d0fa0614d08a37699
Kashif <ahmed.kashif1001@gmail.com>
2025-05-30 19:25:44 +0500

    backend setup complete

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("The server is running!");
+  res.send("Backend setup is complete!");
 });
 
 app.listen(PORT, () => {
